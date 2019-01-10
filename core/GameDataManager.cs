89a8aa733e1ec0ea616f63972b9dea88e9a29bf1
geyaxin <geyaxin@xindong.com>
2019-01-10 21:50:11 +0800

    save

---
+++ core/GameDataManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameDataManager : MonoBehaviour
+{
+    //player data
+    //hero data
+    //enhance data
+       
+}
