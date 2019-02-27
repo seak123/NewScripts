d8048dff8157cfb73bdbe94a8adf1b8ee738da06
seak123 <840557147@qq.com>
2019-02-27 23:29:12 +0800

    save

--- Dungeon/Site/MapSite.cs
+++ Dungeon/Site/MapSite.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public enum SiteType{
     Wilds = 4,
@@ -11,7 +12,7 @@ public class MapSite
 {
     private List<BaseSceneUnit> sceneUnits;
     public static MapSite SiteCreator(){
-        MapSite newSite;
+        MapSite newSite = null;
         return newSite;
     }
 }
